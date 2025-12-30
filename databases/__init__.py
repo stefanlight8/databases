46236c984b5ea3ea6a46b87d6027f5f7acf3e8b0
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2025-12-30 06:43:26 +0300

    __version__ won't be typed

--- databases/__init__.py
+++ databases/__init__.py
@@ -1,5 +1,4 @@
-from typing import Final
 from databases.core import Database, DatabaseURL
 
-__version__: Final[str] = "0.9.0"
+__version__ = "0.9.0"
 __all__: tuple[str, ...] = ("Database", "DatabaseURL")
